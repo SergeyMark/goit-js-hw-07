bb8aaccbbd083c1fd8b7ed0389a93c16cdebaa39
Sergey <110375004+SergeyMark@users.noreply.github.com>
2022-12-13 12:24:36 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(galleryItems.original)
+
+const gallery = document.querySelector('.gallery');
+// console.log(gallery);
+
+const divItem = document.createElement('div');
+divItem.classList.add('gallery__item');
+console.log(divItem);
+
+const aItem = document.querySelector('a');
+aItem.classList.add('gallery__link');
+aItem.setAttribute('href', 'large-image.jpg');
+console.log(aItem);
+
+// const imgItem = document.querySelector('img');
+// // imgItem.classList.add('gallery__image');
+// // imgItem.src = '';
+// imgItem.setAttribute('data-source', 'large-image.jpg');
+// imgItem.setAttribute('alt', 'Image description');
+// console.log(imgItem);
+
+
