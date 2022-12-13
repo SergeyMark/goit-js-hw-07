c3101e3d4f376aef6b875a1356b8d2c68ad42da4
Sergey <110375004+SergeyMark@users.noreply.github.com>
2022-12-13 23:19:51 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,12 +6,15 @@ console.log(galleryItems);
 const galleryTop = document.querySelector('.gallery');
 
 const galleryAll = galleryItems.map((item) => {
-    `<div class="gallery__item">
-     <a class="gallery__link" href="${item.original}">
-     <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/>
-     </a>
-     </div>`
+   console.log(item.description)
+    // `<div class="gallery__item">
+    //  <a class="gallery__link" href="${item.original}">
+    //  <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/>
+    //  </a>
+    //  </div>`
 });
+
+console.log(galleryAll);
 
 galleryTop.insertAdjacentHTML('afterbegin', galleryAll);
 
