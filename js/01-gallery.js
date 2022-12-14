7e29b3744351f9098b783a10c32355aa2ae108ff
Sergey <110375004+SergeyMark@users.noreply.github.com>
2022-12-14 09:28:56 +0200

    up

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryTop = document.querySelector('.gallery');
 
-const galleryAll = galleryItems.map((item) => {
-   console.log(item.description)
-    // `<div class="gallery__item">
-    //  <a class="gallery__link" href="${item.original}">
-    //  <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/>
-    //  </a>
-    //  </div>`
-});
+const galleryAll = galleryItems.map((item) => 
+   `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/>
+        </a>
+    </div>`
+);
+
+
 
 console.log(galleryAll);
 
